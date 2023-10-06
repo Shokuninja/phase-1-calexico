ede40fc8a9eb37ac2a90fcd261e9efbbd19bd923
Lohan <ltwm1998@gmail.com>
2023-10-06 17:39:14 -0400

    update mock code challenge

--- index.js
+++ index.js
@@ -8,13 +8,27 @@ fetch('http://localhost:3000/menu')
             span.textContent = menuItem.name;
             let menuItemsDiv = document.getElementById('menu-items');
             menuItemsDiv.appendChild(span);
-            // console.log(menuItemsDiv);
-
-            console.log(menuItem.description);
-
-
-
-            
         })
+        // Challenge #2
+        console.log(data[0]);
+        let menuItemImage = data[0].image;
+        let menuItemName = data[0].name;
+        let menuItemDescription = data[0].description;
+        let menuItemPrice = data[0].price;
 
+            // Display Image
+        let imageElementDiv = document.getElementById('dish-image');
+        imageElementDiv.src = menuItemImage;
+
+            // Display Name
+        let nameElementDiv = document.getElementById('dish-name');
+        nameElementDiv.innerText = menuItemName;
+
+            // Display Description
+        let descriptionElementDiv = document.getElementById('dish-description');
+        descriptionElementDiv.innerText = menuItemDescription;
+
+            // Display Price
+        let priceElementDiv = document.getElementById('dish-price');
+        priceElementDiv.innerText = `$${menuItemPrice}`;
     })
