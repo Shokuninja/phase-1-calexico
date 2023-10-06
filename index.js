270672ae7533c5bd4cba181c1874ec76fcda9c96
Lohan <ltwm1998@gmail.com>
2023-10-06 14:22:41 -0400

    complete labs

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here...
+
+fetch('http://localhost:3000/menu')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(menuItem => {
+            let span = document.createElement('span');
+            span.textContent = menuItem.name;
+            let menuItemsDiv = document.getElementById('menu-items');
+            menuItemsDiv.appendChild(span);
+            // console.log(menuItemsDiv);
+
+            console.log(menuItem);
+
+
+
+            
+        })
+
+    })
